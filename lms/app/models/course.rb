e7495f4dc192ce38321e2e49399338f502a9c88e
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-06-05 22:02:05 +0530

    Added some relatioship between the coures

---
+++ lms/app/models/course.rb
@@ -0,0 +1,4 @@
+class Course < ActiveRecord::Base
+  has_many :versions, dependent :destroy
+
+end
---
+++ lms/app/models/version.rb
@@ -0,0 +1,8 @@
+class Version < ActiveRecord::Base
+  belongs_to :course
+  belongs_to :category
+  has_many :course_sections, dependent: :destroy
+  has_many :resources, dependent: :destroy
+  has_many :evaluation_questions, dependent: :destory
+
+end
