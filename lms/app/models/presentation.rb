f2265e7e660d5ecc728b9629b7c3a50eb69acc1a
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-11-01 08:24:10 +0530

    Added some more code for presentation

--- lms/app/models/presentation.rb
+++ lms/app/models/presentation.rb
@@ -1,5 +1,6 @@
 class Presentation < ApplicationRecord
   has_many :slides, dependent: :destroy
   belongs_to :course_section
-
+  accepts_nested_attributes_for :slides
+  include CourseContent
 end
