77f4feb445d7e3961eadffaa9722125d922a6d84
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-11-12 20:09:17 +0530

    Slide setting html

--- lms/app/helpers/slide_helper.rb
+++ lms/app/helpers/slide_helper.rb
@@ -2,4 +2,24 @@ module SlideHelper
   def slide_title
     @slide.title && @slide.title.html_safe
   end
+
+  def set_background_color
+    "background-color: #{@slide_setting.background_color}"
+  end
+
+  def background_img_file_name
+    File.basename(@slide_setting.background_img.path) if background_img_exist?
+  end
+
+  def background_img_exist?
+    @slide_setting.background_img.path.present?
+  end
+
+  def bg_img_title_div
+    background_img_exist? || 'hidden-area'
+  end
+
+  def active_transition(transition)
+    @slide_setting.transition.eql?(transition) ? 'active-transition' : ''
+  end
 end
